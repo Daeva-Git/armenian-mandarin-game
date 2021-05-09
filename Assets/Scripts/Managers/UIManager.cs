809ba09d2333639b7df0b67cf4c4fa5999103af6
Daeva <38132564+Daeva-Git@users.noreply.github.com>
2021-05-09 20:16:17 +0400

    Revert "Added animated text displayer"

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,46 +7,24 @@ namespace DefaultNamespace.Managers
     public class UIManager : MonoBehaviour
     {
         [SerializeField] private Canvas canvas;
-        [SerializeField] private Image backgroundPanel;  
-        
         [SerializeField] private Text displayedText;
-        [SerializeField] private Image textPanel;
-        
-        
-        [SerializeField] private Text speakerName;
-        [SerializeField] private Image speakerNamePanel;
+        [SerializeField] private Image backgroundPanel;  
+        [SerializeField] private Image textPanel;  
         
         private void Start()
         {
             backgroundPanel.gameObject.SetActive(false);
             textPanel.gameObject.SetActive(false);
-            speakerNamePanel.gameObject.SetActive(false);
             
-            LoadText(3);
+            LoadText(0);
         }
 
         public void LoadText(int id)
         {
-            TextLine textLine = GameManager.Instance.ComponentManager.TextLines[id];
-            var textToDisplay = textLine.Text;
-            speakerName.text = textLine.SpeakerName;
+            displayedText.text = GameManager.Instance.ComponentManager.TextLines[id].Text;
             
             backgroundPanel.gameObject.SetActive(true);
             textPanel.gameObject.SetActive(true);
-            speakerNamePanel.gameObject.SetActive(true);
-            
-            StartCoroutine(DisplayText(0.3f, textToDisplay));
-        }
-        
-        private IEnumerator DisplayText(float waitTime, string text)
-        {
-            var currentText = "";
-            foreach (var nextChar in text)
-            {
-                currentText += nextChar;
-                displayedText.text = currentText;
-                yield return new WaitForSeconds(waitTime);
-            }
         }
     }
 }
